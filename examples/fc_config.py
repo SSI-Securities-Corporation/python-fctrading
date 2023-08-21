222486c29835a5df46b74f03411131e9c1dfb4ff
Do Van Duc <ducdv@ssi.com.vn>
2023-08-21 13:34:53 +0700

    example config with file ini

--- examples/fc_config.py
+++ examples/fc_config.py
@@ -1,7 +1,13 @@
-ConsumerID = ""
-ConsumerSecret = ""
-PrivateKey = ""
-Url = "https://fc-tradeapi.ssi.com.vn/"
-StreamURL = "https://fc-tradehub.ssi.com.vn/"
-TwoFAType = 0 # 0-PIN, 1-OTP
-NotifyId = '-1'
+import configparser
+import os
+# READ CONFIG
+config = configparser.SafeConfigParser(os.environ)
+config.read(os.path.dirname(os.path.abspath(__file__)) + "/config.ini")
+
+ConsumerID     = config.get("DEFAULT", "ConsumerID", fallback="")
+ConsumerSecret = config.get("DEFAULT", "ConsumerSecret", fallback="")
+PrivateKey     = config.get("DEFAULT", "PrivateKey", fallback="")
+Url            = config.get("DEFAULT", "Url", fallback="https://fc-tradeapi.ssi.com.vn/") 
+StreamURL      = config.get("DEFAULT", "StreamURL", fallback="https://fc-tradehub.ssi.com.vn/")
+TwoFAType      = int(config.get("DEFAULT", "TwoFAType", fallback="0")) # 0-PIN, 1-OTP
+NotifyId       = config.get("DEFAULT", "NotifyId", fallback="-1")
