d07132109ee06d9146d967b41cc39f8f96c8ae0e
Do Van Duc <ducdv@ssi.com.vn>
2024-06-05 13:51:40 +0700

    add on_close on_open delegate

--- examples/fc_stream_ex.py
+++ examples/fc_stream_ex.py
@@ -7,24 +7,26 @@ from . import fc_config
 
 
 def on_message(tapi_message):
-	print("fc_received: " + tapi_message)
+    print("fc_received: " + tapi_message)
 
 
 def on_error(tapi_error):
-	print("fc_error: " + tapi_error)
+    print("fc_error: " + tapi_error)
 
+def on_open():
+    print('Connected to ' + fc_config.StreamURL)
 
 def tapi_data_streaming(on_message, on_error):
-	client = FCTradingClient(fc_config.Url, fc_config.ConsumerID,
-	fc_config.ConsumerSecret, fc_config.PrivateKey, fc_config.TwoFAType)
-	print("access_token: "+ client.get_access_token())
-	stream_client = FCTradingStream(client, fc_config.StreamURL, on_message, on_error, fc_config.NotifyId)
-	stream_client.start()
-	message = None
-	while message != "exit()":
-		message = input(">> ")
+    client = FCTradingClient(fc_config.Url, fc_config.ConsumerID,
+                             fc_config.ConsumerSecret, fc_config.PrivateKey, fc_config.TwoFAType)
+    print("access_token: " + client.get_access_token())
+    stream_client = FCTradingStream(client, fc_config.StreamURL, on_message, on_error, fc_config.NotifyId, on_open=on_open)
+    stream_client.start()
+    message = None
+    while message != "exit()":
+        message = input(">> ")
 
 
 # main function
 if __name__ == '__main__':
-	tapi_data_streaming(on_message, on_error)
+    tapi_data_streaming(on_message, on_error)
